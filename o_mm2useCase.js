08c93b336353558166456bfa66b9da1fab0477ef
shima <t.oshima@jmtech.co.jp>
2017-08-14 18:01:05 +0900

    とりあえず追加したけど・・以下問題あり。 Mindmap内で同一名称の内容をUseCsse側で弾いていない

--- o_mm2useCase.js
+++ o_mm2useCase.js
@@ -1,6 +1,10 @@
 //This script writes out a list of text in the current mindmap.
 //The format is like a WiKi.
 
+// KM1:　Alt＋return の入力は、LF (0x0a)
+// KM2:    そもそも、Macでは、'\', '¥' は使い分けが必要だった・・・・・・ Option + ¥ , ¥ 　Trapだね
+
+
 var depth = 0;
 var INDENT_STR = '  '; //2 spaces
 var ITEM_MARKER_STR = '* ';
@@ -19,8 +23,11 @@ function run() {
             print ("open mindmap and run again.");
             return;
         }
+        var mmRoot = currentDiagram.getRoot();
+        var mmChildren = mmRoot.getChildren();
         var newDiagramName= currentDiagram.getName();
-        
+
+        // MinｄMapと同一名のUseCase図取得
         var projectAccessor = ProjectAccessorFactory.getProjectAccessor();
         var foundUseCaseDiagram = projectAccessor.findElements(IUseCaseDiagram.class, newDiagramName);
         var iuseCaseDiagram = null;
@@ -37,67 +44,85 @@ function run() {
             // use existing UseCaseDiagram
             iuseCaseDiagram =foundUseCaseDiagram[0];
         }
-
-        var existingUseCases = projectAccessor.findElements(IUseCase.class);
-        
-        // 選択中のMindMapを取得
+        var project = astah.getProject();
+        var existingUseCases2 = projectAccessor.findElements(IUseCase.class);
+        var existingUseCases = new Array();
+        for ( var index in existingUseCases2){
+            existingUseCases.push(existingUseCases2[index]);
+        }
+        var selectedEntities = diagramViewManager.getSelectedPresentations();
+        var dEditor = astah.getDiagramEditorFactory().getUseCaseDiagramEditor();
+        var mEditor = astah.getModelEditorFactory().getUseCaseModelEditor();
+        var existingPresentations = iuseCaseDiagram.getPresentations();
+        var initialPoint2D = new java.awt.geom.Point2D.Double(10.0, existingPresentations[0].getHeight());
         with(new JavaImporter(
-                com.change_vision.jude.api.inf.editor,
-                java.awt.geom )) {
-            var project = astah.getProject();
+                com.change_vision.jude.api.inf.editor)) {
             TransactionManager.beginTransaction();
-            var editor = astah.getDiagramEditorFactory().getUseCaseDiagramEditor();
-            var mEditor = astah.getModelEditorFactory().getUseCaseModelEditor();
-            var selectedEntities = diagramViewManager.getSelectedPresentations();
-            var presentations = iuseCaseDiagram.getPresentations();
- 
-            print(presentations);
-            var iUseCase = null;
-            var pointX = 10.0;
-            for ( var sIndex in selectedEntities){
-                var sItem = selectedEntities[sIndex];
-                print(sItem + " in " +  presentations.length + " @ " + iuseCaseDiagram);
-                // MindMapからUseCaseを追加：モデルの追加
-                if (hasItem(existingUseCases, sItem)){
-                    print("already existed : " + sItem);
-                    iUseCase = getItem(existingUseCases, sItem);
-                    // do nothing cause of existing as UseCase
-                }else {
-                    //print("add");
-                    editor.setDiagram(iuseCaseDiagram);
-                    iUseCase = mEditor.createUseCase(project, sItem);
-                }
-                // 対象ユースケース図上への追加：表示の追加
-                if (hasPresentation(presentations, sItem)){
-                    print("already existed as presentation : " + sItem);
-                }else {
-                    var uItem = editor.createNodePresentation(iUseCase, new Point2D.Double(pointX, 10.0));
-                    pointX += 120.0;
-                }
-            }
+            createUseCase(project, mmRoot, selectedEntities, existingUseCases, existingPresentations, dEditor, mEditor, initialPoint2D);
             TransactionManager.endTransaction();
         }
-        return;
-        // 対象UseCase図の内容を取得
-        for ( index in presentations){
-            print(presentations[index]);
-        }
-        var inamed =currentDiagram instanceof INamedElement
-        if (inamed != null){
-            print(inamed);
-        }
-
-
-//        var projectViewManager = astah.getViewManager().getProjectViewManager();
-        
-
-    
-        depth = 0;
-//        printTopics(rootTopic);
     }
 }
+function createUseCase(project, mmItem, selections, existingUseCases, existingPresentations, diagEditor, modelEditor, point){
+    var mmChildren = mmItem.getChildren();
+    var currentPoint = new java.awt.geom.Point2D.Double(point.getX() + 150.0, existingPresentations[0].getHeight());
+//    print(mmChildren + " : " + mmChildren.length);
+    for(var mmIndex in mmChildren){
+        var mmChild = mmChildren[mmIndex];
+        createUseCase(project, mmChild, selections, existingUseCases, existingPresentations, diagEditor,  modelEditor, currentPoint);
+        // 選択中のMindMap要素のみに処理
+        var str = new String(mmChild);
+        if (str.match(/\r?\n/) == null){
+            if (hasPresentation(selections, mmChild)){
+                print("selected " + mmChild );
+                
+                // mindmap に対応したUsecaseModelを取得or 生成
+                var ucChild = null;
+                if (hasItem(existingUseCases, mmChild)){
+                    ucChild = getItem(existingUseCases, mmChild);
+                }else {
+                    print("hage");
+                    ucChild = modelEditor.createUseCase(project, mmChild);
+                    existingUseCases.push(mmChild);
+                }
+                
+                    // ユースケース図上に表示追加:  抑制する場合にはこれが必要
+                    //            if (hasPresentation(existingPresentations, mmChild)){
+                    print("hoge");
 
+                var uItem = diagEditor.createNodePresentation(ucChild, currentPoint);
+                                    print("hige");
+
+              
+            }else{
+                //print("not selected " + mmChild );
+            }
+        }else{
+            print(str + " is not executed cause of return code existed.");
+        }
+                currentPoint.setLocation(currentPoint.getX(), existingPresentations[0].getHeight());
+          
+    }
+}
+function printAllPresentations(_presentations){
+    for (var index in _presentations){
+        print(_presentations[index]);
+    }
+}
 function printTopics(topic) {
+    print(topic[0].getProperties());
+    print(topic + " at " + depth);
+//    var topics = topic[0].getChildren();
+    depth++;
+    for (var i in topic) {
+        if (topic[i].getType() == 'Topic') { //skip MMBoundary
+            printTopics(topic[i]);
+        }
+    }
+    depth--;
+}
+
+function printTopicsOriginal(topic) {
     var topicLabel = topic.getLabel().replaceAll('\n', ' ');
     print(getIndent(depth) + ITEM_MARKER_STR + topicLabel);
 
@@ -136,7 +161,16 @@ function hasPresentation(array, name){
     }
     return false;
 }
+
 function hasItem(array, name){
+    for (var index in array)  {
+        if (array[index].getName() ==  name) {
+            return true;
+        }
+    }
+    return false;
+}
+function hasItem2(array, name){
     //print(typeof(array) + " / " + array.length + " : " + name);
     for (var index in array)  {
         //print(String(array[index]) + " vs " + name);
@@ -150,3 +184,41 @@ function hasClass(element, className){
     return (' ' + element.className + ' ').replace(/[¥n¥t]/g, ' ').indexOf(' ' + className + ' ') !== -1;
 }
 
+//// how to make a class as Javascript
+// www.yunabe.jp/docs/javascript_class_in_google.html
+Person = function(name, age) {
+    this.name = name;
+    this.age = age;
+}
+// define methods by prototype in constructor
+Person.prototype.getName = function() {
+    return this.name;
+}
+Person.prototype.sayHello = function() {
+    print("Hello I¥'m " + this.getName());
+}
+// how to use the above class
+function howToUseAboveClass() {
+    var alice = new Person("Alice", 7);
+    alice.sayHello();
+}
+// inherits
+function howToInherits(){
+    var inherits = function(childCZtor, parentCtor) {
+        // 子クラスのprototypeのプロトタイプとして親クラスのprototypeを指定することで継承が実現される
+        Object.setPrototypeOf(childCtor.prototype, paretntCtror,prototype);
+        
+    };
+    var Employee = function(name, age, salary) {
+        Person.call(this, name, age);
+        this.salary = salary;
+    }
+    inherits(Employee, Person);
+    Employee.prototype.getSalary = function() {
+        return this.salary;
+    }
+    Eployee.prototype.sayHello = function() {
+        Person.prototype.sayHello.call(this);
+        console.log("Salary is " + this.salary);
+    }
+}
